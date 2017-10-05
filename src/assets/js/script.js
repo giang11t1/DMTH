65d3d521b42da6c16f5bca77d8f597822dd26974
HGiang <trangiang11t1@gmail.com>
2017-10-05 08:23:28 +0700

    create page 72

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -858,7 +858,21 @@ function doSomething(e) {
     }
 }
 doSomething();
-
+// tab script================
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    document.getElementById(cityName).className += " active";
+    evt.currentTarget.className += " active";
+}
 
 // thien hoa
 $(document).ready(function() {
